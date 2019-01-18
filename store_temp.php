5dd18690d834aa2ac16d8cac20c14d3c952011e6
Unknown <riviere-stephane@bbox.fr>
2019-01-18 10:18:45 +0100

    Import

---
+++ store_temp.php
@@ -0,0 +1,20 @@
+<?php
+
+$data_json = file_get_contents("php://input");
+$filename_temperature = "$data_json";
+
+//verification du json
+$data = json_decode($data_json);
+if (! $data) {
+	http_response_code(415);
+	exit();
+} elseif (! $data->temperature || ! $data->humidite) {
+	http_response_code(400);
+	exit();
+
+}
+
+$op = file_put_contents($filename_temperature, $data_json);
+if (! $op) {
+	http_response_code(500);
+}
